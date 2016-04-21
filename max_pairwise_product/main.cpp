5b31dbb8930da1380af500cfe02730effe59f465
andreskwan <andreskwan@gmail.com>
2016-04-21 14:33:21 -0500

    Initial Commit

---
+++ max_pairwise_product/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  max_pairwise_product
+//
+//  Created by Andres Kwan on 4/21/16.
+//  Copyright © 2016 Kwan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
