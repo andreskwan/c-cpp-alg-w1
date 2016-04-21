6523b324aa45ffac7d3112c3e26c1648d936fe5e
andreskwan <andreskwan@gmail.com>
2016-04-21 14:59:49 -0500

    AT [w1 - first commit]

--- max_pairwise_product/main.cpp
+++ max_pairwise_product/main.cpp
@@ -1,15 +1,32 @@
-//
-//  main.cpp
-//  max_pairwise_product
-//
-//  Created by Andres Kwan on 4/21/16.
-//  Copyright © 2016 Kwan. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using std::vector;
+using std::cin;
+using std::cout;
+
+long long MaxPairwiseProduct(const vector<int>& numbers) {
+    long long result = 0;
+    int n = numbers.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (((long long)numbers[i]) * numbers[j] > result) {
+                result = ((long long)numbers[i]) * numbers[j];
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+    
+    long long result = MaxPairwiseProduct(numbers);
+    cout << result << "\n";
     return 0;
 }
