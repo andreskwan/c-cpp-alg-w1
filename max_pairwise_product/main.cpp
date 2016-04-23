5df2cfffaf068c40f5c6a2b044d6030a59a6182d
andreskwan <andreskwan@gmail.com>
2016-04-23 14:33:17 -0500

    AT - [w1 - stress testing] identified bug for fast solution, when input sequence contains repeated numbers

--- max_pairwise_product/main.cpp
+++ max_pairwise_product/main.cpp
@@ -35,16 +35,44 @@ long long MaxPairwiseProductFast(const vector<int>& numbers) {
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> numbers[i];
+    while (true){
+        // generate the length of the input sequence
+        int n = rand() % 5 + 2;
+        std::cerr << n << '\n';
+        // generate the array
+        // 1 create the dinami array
+        vector<int> inputSequence;
+        // 2 fill up the vector with random numbers
+        for (int i = 0; i < n; i++) {
+            inputSequence.push_back(rand() % 10);
+        }
+        for (int j = 0; j < n; j++) {
+            std::cerr << inputSequence[j] << ' ';
+        }
+        std::cerr << "\n";
+        // 3 generate output for fast and slow solutions
+        long long result1 = MaxPairwiseProduct(inputSequence);
+        long long result2 = MaxPairwiseProductFast(inputSequence);
+        
+        
+        if (result1 != result2) {
+            std::cerr << "Wrong answer: slow= " << result1 << " fast= " << result2 << '\n';
+            break;
+        } else {
+            std::cerr << "OK \n";
+        }
     }
-    
-//    long long result1 = MaxPairwiseProduct(numbers);
-    long long result1 = MaxPairwiseProductFast(numbers);
-    cout << result1;
+    //Commented to run stress testing
+//    int n;
+//    cin >> n;
+//    vector<int> numbers(n);
+//    for (int i = 0; i < n; ++i) {
+//        cin >> numbers[i];
+//    }
+//    
+////    long long result1 = MaxPairwiseProduct(numbers);
+//    long long result1 = MaxPairwiseProductFast(numbers);
+//    cout << result1;
 
     return 0;
 }
